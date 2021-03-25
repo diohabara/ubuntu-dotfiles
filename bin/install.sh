f57373d45ab35fcab0c951339d57b5ee794902da
diohabara <diohabara@gmail.com>
2021-03-25 20:48:33 +0900

    fix: remove invalid option

--- bin/install.sh
+++ bin/install.sh
@@ -142,7 +142,7 @@ function already() {
 : "install visual studio code" && {
   # Doc: https://code.visualstudio.com/docs/setup/linux
   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-  sudo install -y -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
   sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
   sudo apt install apt-transport-https
   sudo apt update
