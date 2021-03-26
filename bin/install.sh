8170d0f3ab94766dbc86d81a776cface033b1e04
diohabara <diohabara@gmail.com>
2021-03-26 13:47:36 +0900

    fix: not gonna use snap

--- bin/install.sh
+++ bin/install.sh
@@ -88,10 +88,6 @@ function already() {
                     zsh
 }
 
-: "install packages by snap" && {
-  sudo snap install alacritty --classic
-}
-
 : "install nix" && {
   if ! command_exists nix-env; then
     installing 'nix'
@@ -168,6 +164,25 @@ function already() {
   if ! command_exists starship; then
     # Doc: https://starship.rs/
     curl -fsSL https://starship.rs/install.sh | bash
+  fi
+}
+
+: "install alacritty" && {
+  if ! command_exists alacritty; then
+    # Doc: https://github.com/alacritty/alacritty/blob/master/INSTALL.md#desktop-entry
+    git clone https://github.com/alacritty/alacritty.git
+    cd alacritty
+    rustup override set stable
+    rustup update stable
+    apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+    cargo build --release
+    sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+    sudo mkdir -p /usr/local/share/man/man1
+    gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
   fi
 }
 
