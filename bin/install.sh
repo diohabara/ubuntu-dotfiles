729b250af947e59b0d047871e62d3242a458825d
diohabara <diohabara@gmail.com>
2021-03-28 18:28:49 +0900

    fix: correct typo

--- bin/install.sh
+++ bin/install.sh
@@ -144,7 +144,7 @@ function already() {
 }
 
 : "install Google Chrome" && {
-  if ! comamnd_exists google-chrome; then
+  if ! command_exists google-chrome; then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo apt install ./google-chrome-stable_current_amd64.deb
     rm -rf google-chrome-stable_current_amd64.deb
