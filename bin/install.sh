9254cb789e3a5c85363a9b62d5cc19be49540f45
Kadoi Takemaru <diohabara@gmail.com>
2021-03-28 17:07:39 +0900

    fix syntax error

--- bin/install.sh
+++ bin/install.sh
@@ -151,7 +151,7 @@ function already() {
 }
 
 : "install Doom Emacs" && {
-  if ! [ -d "$HOME/.emacs.d ]; then
+  if ! [ -d "$HOME/.emacs.d" ]; then
     git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
     ~/.emacs.d/bin/doom install
   fi
