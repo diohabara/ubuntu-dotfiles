2a1521f3cf6d669060814fac9acdddc6403acdd6
diohabara <diohabara@gmail.com>
2021-04-01 18:47:10 +0900

    fix: won't use common lisp

--- dotfiles/.config/doom/config.el
+++ dotfiles/.config/doom/config.el
@@ -5,8 +5,6 @@
 (setq user-full-name "Kadoi Takemaru"
       user-mail-address "diohabara@gmail.com")
 
-
-
 (setq doom-theme 'doom-one)
 
 (setq display-line-numbers-type 'relative)
@@ -47,8 +45,6 @@
 (after! ccls
   (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
   (set-lsp-priority! 'ccls 2)) ; optional as ccls is the default in Doom
-
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
 
 (after! lsp-rust
   (setq lsp-rust-server 'rust-analyzer))
