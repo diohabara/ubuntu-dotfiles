7488e5e0de551325e65a8839830c4d071fc33699
diohabara <diohabara@gmail.com>
2021-04-24 22:02:28 +0900

    fix: update for atcodertools

--- bin/install.sh
+++ bin/install.sh
@@ -140,18 +140,6 @@ function already() {
   fi
 }
 
-: "install visual studio code" && {
-  if ! command_exists code; then
-    # Doc: https://code.visualstudio.com/docs/setup/linux
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo apt install apt-transport-https
-    sudo apt update
-    sudo apt install -y code # or code-insiders
-  fi
-}
-
 : "install Google Chrome" && {
   if ! command_exists google-chrome; then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -217,6 +205,7 @@ function already() {
       pip3 install pytest
       pip3 install python-language-server[all]
       pip3 install wakatime
+      pip3 install atcoder-tools
     fi
   }
 }
