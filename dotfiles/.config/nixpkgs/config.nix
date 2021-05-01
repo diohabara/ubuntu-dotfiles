3168e097f81c269020749a62bb71876332eabad1
diohabara <diohabara@gmail.com>
2021-05-01 11:56:03 +0900

    refactor: format nix files

--- dotfiles/.config/nixpkgs/config.nix
+++ dotfiles/.config/nixpkgs/config.nix
@@ -1,23 +1,21 @@
 {
   allowUnfree = true;
-  packageOverrides = pkgs: with pkgs; rec {
-    myPackages = pkgs.buildEnv {
-      name = "my-packages";
-      paths = [
-        nixfmt
-        typora
+  packageOverrides = pkgs:
+    with pkgs; rec {
+      myPackages = pkgs.buildEnv {
+        name = "my-packages";
+        paths = [ nixfmt typora ];
+        extraOutputsToInstall = [ "man" "doc" "info" ];
+      };
+      fonts.fonts = with pkgs; [
+        cascadia-code
+        dejavu_fonts
+        jetbrains-mono
+        nerdfonts
+        noto-fonts-cjk
+        powerline-fonts
+        roboto
+        source-code-pro
       ];
-      extraOutputsToInstall = [ "man" "doc" "info" ];
     };
-    fonts.fonts = with pkgs; [
-      cascadia-code
-      dejavu_fonts
-      jetbrains-mono
-      nerdfonts
-      noto-fonts-cjk
-      powerline-fonts
-      roboto
-      source-code-pro
-    ];
-  };
 }
